dc7409a0274a14325408cc57cdb4ce0f2a4a1c26
Sam Mulube <sam@umbrellium.co.uk>
2015-08-21 01:15:25 +0100

    Deploy failed on verify branch step - remove the valid path

--- run.sh
+++ run.sh
@@ -3,10 +3,7 @@ if [ ! -n "$WERCKER_VERIFY_BRANCH_BRANCH" ]; then
   exit 1
 fi
 
-if [ "$WERCKER_VERIFY_BRANCH_BRANCH" == "$WERCKER_GIT_BRANCH" ]; then
-  echo 'Valid branch'
-  exit 0
-else
+if [ "$WERCKER_VERIFY_BRANCH_BRANCH" != "$WERCKER_GIT_BRANCH" ]; then
   error 'Invalid branch detected'
   exit 1
 fi
